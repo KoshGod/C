ce49f6c34b0a0ca811d603bbcba42e87b99745bc
KoshGod <161854826+KoshGod@users.noreply.github.com>
2024-03-01 18:08:48 +0300

    Add files via upload

---
+++ dz++2.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+#include <string>
+
+void calculateLetterFrequency(const std::string& str) {
+    int frequencyMap[26] = { 0 };
+
+    for (char c : str) {
+        if (std::isalpha(c) && std::islower(c)) {
+            int index = c - 'a';
+            frequencyMap[index]++;
+        }
+    }
+
+    for (int i = 0; i < 26; ++i) {
+        if (frequencyMap[i] > 0) {
+            char letter = 'a' + i;
+            std::cout << letter << ": " << frequencyMap[i] << std::endl;
+        }
+    }
+}
+
+int main() {
+    std::string inputString = "Hello, World!";
+    calculateLetterFrequency(inputString);
+
+    return 0;
+}
